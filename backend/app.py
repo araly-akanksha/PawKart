f204fb66133026041a6f35aa71a395eaba6cb0b8
araly-akanksha <aralyakankshanaidu@gmail.com>
2026-06-01 13:02:15 +0530

    backend Completed

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,16 @@
-# app.py
-
 from fastapi import FastAPI
+from product_routes import router as product_router
 
 app = FastAPI(
     title="PawKart API",
     version="1.0"
 )
 
+app.include_router(product_router)
+
+
 @app.get("/")
 def home():
-    return {"message": "PawKart Backend Running"}
+    return {
+        "message": "PawKart Backend Running"
+    }
---
+++ backend/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    product_name = Column(String)
+    category = Column(String)
+    price = Column(Float)
