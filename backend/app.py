ef00c8e1daf95636ac22f110d6e5ecf5b111b636
AMRUTHA M <amrutha.m63644@gmail.com>
2026-06-02 11:46:20 +0530

    product forecasting

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from product_routes import router as product_router
 from inventory_routes import router as inventory_router
 from rfid_routes import router as rfid_router
+import forecasting_routes
 
 app = FastAPI(
     title="PawKart API",
@@ -11,6 +12,7 @@ app = FastAPI(
 app.include_router(product_router)
 app.include_router(inventory_router)
 app.include_router(rfid_router)
+app.include_router(forecasting_routes.router)
 
 @app.get("/")
 def home():
